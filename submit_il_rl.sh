2f52dc9c5d75f9c160656615995090a26d451ab7
cxiangood <1504047409@qq.com>
2026-03-14 16:20:01 +0800

    fix-wrong-match

--- submit_il_rl.sh
+++ submit_il_rl.sh
@@ -55,6 +55,7 @@ python3 dreamer/dreamerv3/main.py \
   --run.envs 1 \
   --run.eval_envs 1 \
   --batch_size 16 \
+  --env.metadrive.expert_execute True \
   --env.metadrive.expert_heads 1 \
   --env.metadrive.expert_modes 1 \
   $ENV_METADRIVE_SIZE_ARG \
